8263da114e6e7e5f8971f1f1ae17baa4d99c839c
Madhav Sharma <77728555+Madhavgiga1@users.noreply.github.com>
2023-06-22 23:57:37 +0530

    roud150code

---
+++ edround150.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+void solveboard(int n){
+	if(n>4){
+		cout<<"Alice"<<endl;
+	}
+	else{
+		cout<<"Bob"<<endl;
+	}
+}
+void solvebeautiful(){
+	int q;
+	cin>>q;
+	deque<int> a;
+	while(q--){
+		   /*vector<long> a(q);
+		    for(int i = 0; i < q; i++) {
+		        cin >> a[i];
+		    }
+		    vector<int> ans(q, 0);
+		    int ind = -1;
+		    for(int i = q - 2; i >= 0; i--) {
+		        if(a[i] > a[i+1]) {
+		            break;
+		        }
+		        ind = i;
+		    }
+		    if(ind < 0) {
+		        cout << 1 << " ";
+		        return;
+		    }
+		    bool canbeautiful = true;
+		    for(int i = 0; i < ind; i++) {
+		        if(a[i] > a[i+1]) {
+		            canbeautiful = false;
+		            break;
+		        }
+		    }
+		    if(canbeautiful) {
+		        if(a[q-1] <= a[0]) {
+		            cout << 1 << " ";
+		        }
+		        else {
+		            cout << 0 << " ";
+		        }
+		    }
+		    else {
+		        cout << 0 << " ";
+		    }
+		    cout << endl;*/
+		    
+		    cin>>p;
+		    if
+	}
+	
+}
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+    	
+    	solvebeautiful();
+
+    }
+    return 0;
+}
