7be79c3a232e142805ec3c5382b7fb1df0339535
Madhav Sharma <77728555+Madhavgiga1@users.noreply.github.com>
2023-06-22 23:59:32 +0530

    chcontests

---
+++ checkering.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+	#ifndef ONLINE_JUDGE
+	freopen("input1.txt", "r", stdin);
+	freopen("output1.txt", "w", stdout);
+#endif
+	int n;
+	cin>>n;
+	for(int i=0;i<n;i++){
+		cout<<i<<" ";
+	}
+	return 0;
+}
