0871986eb38502e700668d6b5d5799cf48089bb2
Madhav Sharma <77728555+Madhavgiga1@users.noreply.github.com>
2023-07-03 23:52:58 +0530

    03

--- leetcodejuly2023daily.cpp
+++ leetcodejuly2023daily.cpp
@@ -28,17 +28,77 @@ bool targetiterative(int index,int target,vector<int>& nums,vector<vector<int>>&
 			if(nums[i]<=j){
 				take=dp[i-1][j-nums[i]];
 			}
-			dp[i][j]=take||notake;
+			dp[i][j]=take||notake;//do not put dp[i][target]=take||notake;
 		}
 	}
 	return dp[index][target];
 }
+void genbinarystringsizek(int i,int k,vector<int>& ds,vector<vector<int>>& ans){
+	if(i==k){
+		ans.push_back(ds);
+		return;
+	}
+	ds.push_back(0);
+	genbinarystringsizek(i+1,k,ds,ans);
+	ds.pop_back();
+	if(i==0){
+		ds.push_back(1);
+		genbinarystringsizek(i+1,k,ds,ans);
+		ds.pop_back();
+	}
+	if(i!=0&& ds[i-1]!=1){
+		ds.push_back(1);
+		genbinarystringsizek(i+1,k,ds,ans);
+		ds.pop_back();
+	}
+}
 int main(){
-	vector<int> nums={2,3,1,9,8};
-	vector<vector<int>> dp(5,vector<int>(24,0));
-	targetiterative(4,20,nums,dp);
-	for(int i=1;i<23;i++){
-		cout<<i<<": "<<dp[4][i]<<endl;
+	//vector<int> nums={2,3,1,9,8};
+	//vector<vector<int>> dp(5,vector<int>(24,0));
+	//targetiterative(4,20,nums,dp);
+	//for(int i=1;i<23;i++){
+	//	cout<<i<<": "<<dp[4][i]<<endl;
+	//}
+	vector<int> ds;
+	vector<vector<int>> ans;
+
+	genbinarystringsizek(0,2,ds,ans);
+	for(auto p:ans){
+		for(auto d:p)cout<<d<<" ";
+		cout<<endl;
 	}
 	return 0;
 }
+/*struct Item {
+    int value, weight;
+    Item(int value, int weight){
+        this->value = value;
+        this->weight = weight;
+    }
+};
+
+bool cmp(struct Item a, struct Item b)
+{
+    double r1 = (double)a.value / (double)a.weight;
+    double r2 = (double)b.value / (double)b.weight;
+    return r1 > r2;
+}
+
+
+double fractionalKnapsack(int W, struct Item arr[], int N){
+    sort(arr, arr + N, cmp);
+    double finalvalue = 0.0;
+    for (int i = 0; i < N; i++) {
+        if (arr[i].weight <= W) {
+            W -= arr[i].weight;
+            finalvalue += arr[i].value;
+        }
+        else {
+            finalvalue
+                += arr[i].value
+                   * ((double)W / (double)arr[i].weight);
+            break;
+        }
+    }
+    return finalvalue;
+}*/
