e27160fae29b5f0eb7d0163b8203522ccac34f3a
Madhav Sharma <77728555+Madhavgiga1@users.noreply.github.com>
2023-09-11 23:53:41 +0530

    ft

--- leetcodejuly2023daily.cpp
+++ leetcodejuly2023daily.cpp
@@ -69,7 +69,7 @@ int main(){
 	}
 	return 0;
 }
-/*struct Item {
+struct Item {
     int value, weight;
     Item(int value, int weight){
         this->value = value;
@@ -101,4 +101,43 @@ double fractionalKnapsack(int W, struct Item arr[], int N){
         }
     }
     return finalvalue;
-}*/
+}
+class Solution {
+public:
+    bool buddyStrings(string s, string goal) {
+        if (s.size() != goal.size()) {
+            return false;
+        }
+        if (s == goal) {
+            vector<int> frequency(26, 0);
+            for (auto& ch : s) {
+                frequency[ch - 'a'] += 1;
+                if (frequency[ch - 'a'] == 2) {
+                    return true;
+                }
+            }
+            return false;
+        }
+        int firstIndex = -1;
+        int secondIndex = -1;
+        for (int i = 0; i < s.size(); ++i) {
+            if (s[i] != goal[i]) {
+                if (firstIndex == -1) {
+                    firstIndex = i;
+                } else if (secondIndex == -1) {
+                    secondIndex = i;
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        if (secondIndex == -1) {
+            
+            return false;
+        }
+       
+        return s[firstIndex] == goal[secondIndex] && 
+               s[secondIndex] == goal[firstIndex];
+    }
+};
